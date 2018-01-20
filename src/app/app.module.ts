e19955dc96c1b5fc726e1380747495e4f5fcd990
Shadab Shamsi <shadablaptop@gmail.com>
2018-01-20 16:46:03 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductBoxComponent } from './product-box/product-box.component';
+import { MenuComponent } from './menu/menu.component';
 
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: "/menu",
+    pathMatch: 'full'
+  },
+  {path : 'menu', component : MenuComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductBoxComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( routes )
   ],
   providers: [],
   bootstrap: [AppComponent]
