b14fb2495d994c07f59252555fb2928e7f89267b
Shadab Shamsi <shadablaptop@gmail.com>
2018-01-27 07:53:32 +0530

    added data-table module

---
+++ src/app/config.ts
@@ -0,0 +1,5 @@
+export var config = {
+    title: "Canteen Management System",
+    serverUrl:"http://localhost:8080/canteenmgmtserver/",
+    imageUrl:"http://localhost:8080/canteenmgmtserver/Assets/Image/"
+  }
